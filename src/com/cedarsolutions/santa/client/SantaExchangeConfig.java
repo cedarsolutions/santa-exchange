d01b86fc40f66f87836c7cf16a2a600ab7683502
pronovic <pronovic@wintermute>
2014-09-26 20:23:26 -0500

    Release 0.9.38.

--- src/com/cedarsolutions/santa/client/SantaExchangeConfig.java
+++ src/com/cedarsolutions/santa/client/SantaExchangeConfig.java
@@ -35,11 +35,11 @@ public interface SantaExchangeConfig extends ConstantsWithLookup {
     String system_applicationName();
 
     /** Application version number. */
-    @DefaultStringValue("0.9.37")
+    @DefaultStringValue("0.9.38")
     String system_versionNumber();
 
     /** Application release date. */
-    @DefaultStringValue("08 Jul 2014")
+    @DefaultStringValue("26 Sep 2014")
     String system_releaseDate();
 
     /** Copyright statement for the application. */
