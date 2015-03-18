810b347d36f8261f2e6eec1f8b89295eca7d4989
Kenneth J. Pronovici <pronovic@ieee.org>
2015-03-18 17:27:48 -0500

    Release v0.9.47.

--- src/com/cedarsolutions/santa/client/SantaExchangeConfig.java
+++ src/com/cedarsolutions/santa/client/SantaExchangeConfig.java
@@ -35,11 +35,11 @@ public interface SantaExchangeConfig extends ConstantsWithLookup {
     String system_applicationName();
 
     /** Application version number. */
-    @DefaultStringValue("0.9.46")
+    @DefaultStringValue("0.9.47")
     String system_versionNumber();
 
     /** Application release date. */
-    @DefaultStringValue("17 Mar 2015")
+    @DefaultStringValue("18 Mar 2015")
     String system_releaseDate();
 
     /** Copyright statement for the application. */
